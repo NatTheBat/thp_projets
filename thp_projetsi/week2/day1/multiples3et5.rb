65807dcca20e8e2039865aaf3beb0cbff474b3e8
Asheraz <lfrely@gmail.comssh-keygen -t rsa -C lfrely@gmail.com>
2017-10-09 12:18:28 +0200

    V2

--- thp_projetsi/week2/day1/multiples3et5.rb
+++ thp_projetsi/week2/day1/multiples3et5.rb
@@ -1,38 +1,35 @@
 def somme_multiples(nombre)
 
-	#Initialiser les tableaux qui contiendront les multiples de 3 et 5 inférieurs à "nombre"
-	multiple_3=[];
-	multiple_5=[];
+	#Initialiser la variable qui contiendra la somme
+	a = 0;
+
+	#Initialiser la variable qui contiendra les multiples de 3 et 5 (ie multiples de 15)
+	b = 0;
 
 	#Limiter la recherche des multiples - ce multiples fois 3 ou 5 ne peut être supérieur à 1000
 	max_multiple_3 = (nombre-1)/3;
 	max_multiple_5 = (nombre-1)/5;
+	max_multiple_15 = (nombre-1)/15;
 
-	#Parcourir les nombres et si c'est un multiple de 3, le rajouter à la liste des multiples de 3
-	1.upto(max_multiple_3) do |y|
-			multiple_3 << y*3;
-	end;
-
-
-	#Parcourir les nombres et si c'est un multiple de 5, le rajouter à la liste des multiples de 3
-	1.upto(max_multiple_5) do |z|
-			multiple_5 << z*5;
-	end;
-
-	#Sommer les multiples de 3 et 5
-
-	total = 0
-
-	multiple_3.each do |z|
-		total +=z
-	end
-
-	multiple_5.each do |z|
-		total+=z
-	end
-
+	#Parcourir les nombres et si c'est un multiple de 3 ou 5, le rajouter à la liste des multiples de 3
 	
-puts total
+	# Optimisation, ne pas regarder si multiple de 5 si sup à max_multiple 5
+	1.upto(max_multiple_5) do |z|
+			a+=z*3;
+			a+=z*5;
+	end;
+
+	((max_multiple_5+1)..max_multiple_3).each do |y|
+			a+=y*3
+	end;
+ 	
+ 	# Calcul multiples de 15
+	1.upto(max_multiple_15) do |x|
+			b+=x*15;
+	end;
+
+
+puts a-b
 
 end;
 
